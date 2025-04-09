b7661d7211c47fcf281c067da8c94a54c26d40e4
JepcDev <joseenrique.pc21@gmail.com>
2025-04-08 22:30:50 -0400

    feat(header): add component section

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+// Un decorador es la manera de extender la funcionalidad de algo
+@Component({
+  // indica como se va a llamar el componente
+  selector: 'app-header',
+  // template:'Kamba desde el mimso componente' ,//Cambia desde el mismo componente no se necesita un html externo
+  templateUrl: './header.component.html',//se refiere al elemento externo html que se va renderizar en este componente
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  // HeaderComponent es en realidad el componente
+  // Los componentes tienen la particularidad que se puede hacer la logica en typescript y se renderize en el html, el proceso de mostrar en pantalla algun resultado es mas sencillo y automatico
+  // OnInit es la primera funcion que se ejecuta una vez que el HTML ya se cargo
+
+  // variable de una clase,(: Date | null) le especificamos que tipo de datos puede almacenar
+  // currentDate: Date | null = new Date();//
+  currentDate: Date | null = null;//
+  resultado: number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // this.currentDate = new Date();
+    // console.log(this.currentDate);
+
+    // const myname = 'jose';
+    // console.log(`Hola ${myname}`);
+    // let suma:number = this.sumar(1,4);
+    // console.log('La suma total es = ',this.sumar(1,4));
+  }
+
+  // una funcion en angular
+  sumar(a:number, b:number){
+    this.resultado = a + b;
+  }
+
+}
--- src/app/info.md
+++ src/app/info.md
@@ -4,3 +4,11 @@ Componente: Es la piesa minima fundamental de logica de programacion. Es una peq
 
 - ng generate component nombreComponente //Crea un componente // se puede usar => ng g c nombreComponente
 -
+
+@NgModulo: Es el encargado de avisarle a angular que existe un componente y que lo debe renderizar.
+
+```'ReadableByteStreamController' is referenced directly or indirectly in its own type annotation.
+492         var ReadableByteStreamController: typeof globalThis extends// "@types/node": "20.11.1",```
+
+Todo lo que se encuentra en el component.ts se puede llamar desde el component.html
+Los eventos que suceden en el html de cualquier cosa se capturan con () y lo que va accionar ese evento es codigo js
