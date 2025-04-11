ea16e2573479059d7e44dfecc97968375032cd52
JepcDev <joseenrique.pc21@gmail.com>
2025-04-10 22:05:25 -0400

    feat: add *ngIf, *ngFor, *ngTempplateOutlet estructural directive

--- src/app/directive/estructural/estructural.component.ts
+++ src/app/directive/estructural/estructural.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstructuralComponent implements OnInit {
 
+  isError = false;
+  sections = [
+    {id:1,name:'Angular',duracion:'10:00'},
+    // {id:1,name:'React',duracion:'5:00'},
+    // {id:1,name:'Vue',duracion:'10:00'},
+    // {id:1,name:'Flutter',duracion:'5:00'},
+    // {id:1,name:'Kotlin',duracion:'10:00'},
+  ];
+  // sections = [
+  //   'Angular',
+  //   'React',
+  //   'Vue',
+  //   'Flutter',
+  //   'Kotlin',
+  // ];
+
   constructor() { }
 
   ngOnInit(): void {
+    // setTimeout(() => {
+    //   this.isError=true;
+    // }, 4000);
+
+    // setTimeout(() => {
+    //   this.sections.push( {id:1,name:'React',duracion:'5:00'});
+    //   this.sections.push( {id:1,name:'Vue',duracion:'10:00'});
+    // }, 4000);
+  }
+
+  showError(){
+    this.isError = true;
+  }
+  hiddenError(){
+    this.isError = false;
+  }
+  showSection(){
+    this.sections.push( {id:1,name:'React',duracion:'5:00'});
+  }
+  hiddenSection(){
+    this.sections.pop( );
   }
 
 }
